2cbdda820423b26e1bf8b59ec5cac1a51dd2d750
JLBBARCO <joseluizbarco@gmail.com>
2025-09-18 16:19:29 -0300

    Layout adicionado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
+const desenvolvedor = "José Luiz Bruiani Barco";
 document.addEventListener("DOMContentLoaded", () => {
-  // * Header
-  const header = document.querySelector("header");
-  header.innerHTML = `<nav class='navbar'></nav><menu></menu>`;
-
   // * Footer
   const currentYear = new Date().getFullYear();
-  document.getElementById("currentYear").textContent = currentYear;
 
-  const footer = document.querySelector("footer");
-  footer.innerHTML = `<section class='copyright'>&copy; ${currentYear} Your Company. All rights reserved.</section>`;
+  const yearSpan = document.getElementById("currentYear");
+  if (yearSpan) {
+    yearSpan.textContent = currentYear;
+  }
+
+  const currentYearHTML = document.querySelector("#currentYear");
+  if (currentYearHTML) {
+    currentYearHTML.innerHTML = currentYear;
+  }
 });
