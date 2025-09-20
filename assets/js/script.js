50987095db33c346b376d4e4f5c129df8c0aeaaf
JLBBARCO <joseluizbarco@gmail.com>
2025-09-20 15:14:33 -0300

    add contatos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,55 @@
 const desenvolvedor = "José Luiz Bruiani Barco";
 document.addEventListener("DOMContentLoaded", () => {
-  // * Footer
-  const currentYear = new Date().getFullYear();
+  // * Header
+  const header = document.querySelector("header");
+  if (header) {
+    header.innerHTML = `
+    <span class="college icon"></span>
+      <section class="location-info">
+        <h1>Colégio Estadual Governador Adolpho de Oliveira Franco - EFMP</h1>
+        <div class="location-info">
+        <p>
+          <span class="location icon"></span>
+          Astorga, Paraná, Brasil
+        </p>
+        </div>
+      </section>`;
+  }
 
+  // * Footer
+  const footer = document.querySelector("footer");
+  if (footer) {
+    footer.innerHTML = `
+      <section class="copyright">
+        &copy; <span id="currentYear"></span>. Site desenvolvido por José Luiz
+        Bruiani Barco.
+      </section>`;
+  }
+
+  // * Current Year
+  const currentYear = new Date().getFullYear();
   const yearSpan = document.getElementById("currentYear");
   if (yearSpan) {
     yearSpan.textContent = currentYear;
   }
-
-  const currentYearHTML = document.querySelector("#currentYear");
-  if (currentYearHTML) {
-    currentYearHTML.innerHTML = currentYear;
-  }
 });
 
-function contato() {
-  document.getElementById("contato").style.display = "flex";
+// * Cards que mudam de acordo com o botão clicado
+const cards = document.querySelectorAll("#mainContent .container");
+
+// * Funções dos Botões de Navegação
+function inícioBotão() {
+  cards.forEach((card) => {
+    card.classList.remove("active");
+  });
+  const containerHome = document.querySelector("#container-home");
+  containerHome.classList.add("active");
+}
+
+function contatoBotão() {
+  cards.forEach((card) => {
+    card.classList.remove("active");
+  });
+  const containerContato = document.querySelector("#container-contato");
+  containerContato.classList.add("active");
 }
--- assets/js/svg.js
+++ assets/js/svg.js
@@ -9,6 +9,12 @@ document.addEventListener("DOMContentLoaded", () => {
   //   * Ícone de Localização
   const locationIcon = document.querySelectorAll("span.location");
   locationIcon.forEach((icon) => {
-    icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin h-8 w-8 text-white" aria-hidden="true"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0Z"></path><circle cx="12" cy="10" r="3"></circle></svg>`;
+    icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin h-8 w-8 text-white" aria-hidden="true"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0Z"></path><circle cx="12" cy="10" r="3"></circle></svg>`;
+  });
+
+  // * Ícone de Telefone
+  const phoneIcon = document.querySelectorAll("span.phone");
+  phoneIcon.forEach((icon) => {
+    icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-phone h-8 w-8 text-white" aria-hidden="true"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6A19.79 19.79 0 0 1 2.11 4.18 2 2 0 0 1 4 2h3a2 2 0 0 1 2 1.72c.13 1.21.37 2.39.7 3.5a2 2 0 0 1-.45 2.11L8.09 10.91a16 16 0 0 0 6 6l1.58-1.58a2 2 0 0 1 2.11-.45c1.11.33 2.29.57 3.5.7a2 2 0 0 1 1.72 2Z"></path></svg>`;
   });
 });
